7833783ef85dbe4519605f3119e3024e859e639c
immprada <im.mprada@gmail.com>
2022-10-21 13:30:02 -0500

    Build a Node class. It should have an attribute for the data it stores as well as its left and right children.

---
+++ lib/tree/node.rb
@@ -0,0 +1,11 @@
+module Tree
+  class Node
+    attr_reader :data, :left_child, :right_child
+
+    def initialize(data, left_child = nil, right_child = nil)
+      @data = data
+      @left_child = left_child
+      @right_child = right_child
+    end
+  end
+end
