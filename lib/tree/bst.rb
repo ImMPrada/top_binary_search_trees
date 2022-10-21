646897df4c05c5291792a7ae4cf5e1651843244b
immprada <im.mprada@gmail.com>
2022-10-21 15:32:11 -0500

    Write a #build_tree method which takes an array of data and turns it into a balanced binary tree full of Node objects appropriately placed. The #build_tree method should return the level-0 root node.

--- lib/tree/bst.rb
+++ lib/tree/bst.rb
@@ -1,10 +1,53 @@
+require_relative 'node'
+
 module Tree
   class BST
+    LEFT_SIDE = :left
+    RIGHT_SIDE = :right
+    BASIC_ARR_SIZE = (2..3)
+
     attr_reader :root
 
     def initialize(arr)
       @sorted_arr = arr.sort.uniq
       @root = nil
+    end
+
+    def build_tree
+      return if @sorted_arr.empty?
+
+      @root = build_tree_helper(@sorted_arr)
+    end
+
+    private
+
+    def build_tree_helper(current_array)
+      return build_basic_node(current_array) if BASIC_ARR_SIZE.include?(current_array.length)
+
+      current_root = current_array[current_array.length / 2]
+
+      Node.new(
+        current_root,
+        build_tree_helper(split_array(current_array, LEFT_SIDE)),
+        build_tree_helper(split_array(current_array, RIGHT_SIDE))
+      )
+    end
+
+    def build_basic_node(current_array)
+      return Node.new(current_array[0], nil, Node.new(current_array[1])) if two_elements_array?(current_array)
+
+      Node.new(current_array[1], Node.new(current_array[0]), Node.new(current_array[2]))
+    end
+
+    def two_elements_array?(current_array)
+      current_array.length == 2
+    end
+
+    def split_array(current_array, side)
+      spliter = current_array.length / 2
+
+      return current_array[0..spliter - 1] if side == LEFT_SIDE
+      return current_array[spliter + 1..] if side == RIGHT_SIDE
     end
   end
 end
