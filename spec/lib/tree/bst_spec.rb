f445056a8967dd27abbb5c61b2a5ae517e10ad96
immprada <im.mprada@gmail.com>
2022-10-23 19:36:23 -0500

    add Trre::BST #find spec

--- spec/lib/tree/bst_spec.rb
+++ spec/lib/tree/bst_spec.rb
@@ -35,4 +35,27 @@ RSpec.describe Tree::BST do
       end
     end
   end
+
+  describe '#find' do
+    let(:a_value_in_the_test_array) { test_array.sample }
+    let(:a_value_not_in_the_test_array) { 100 }
+
+    before { bst.build_tree }
+
+    describe 'when the value is present in the tree' do
+      it 'returns a node' do
+        expect(bst.find(a_value_in_the_test_array).class).to eq(Tree::Node)
+      end
+
+      it 'returns the node with the value' do
+        expect(bst.find(a_value_in_the_test_array).data).to eq(a_value_in_the_test_array)
+      end
+    end
+
+    describe 'when the value is not present in the tree' do
+      it 'returns nil' do
+        expect(bst.find(a_value_not_in_the_test_array)).to be_nil
+      end
+    end
+  end
 end
