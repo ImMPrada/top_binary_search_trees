69f919193a14d4a962fd2036c465fc2f5c29092a
immprada <im.mprada@gmail.com>
2022-10-25 16:33:22 -0500

    add Tree::FakeQueue class

---
+++ lib/tree/fake_queue.rb
@@ -0,0 +1,32 @@
+require_relative 'tree'
+
+module Tree
+  class FakeQueue
+    attr_reader :data
+
+    def initialize(first_data = nil)
+      @data = [first_data]
+    end
+
+    def add(value)
+      @data << value
+    end
+
+    def remove
+      return nil if @data.empty?
+
+      removed_value = @data[0]
+      @data = @data[1..]
+
+      removed_value
+    end
+
+    def empty?
+      @data.empty?
+    end
+
+    def size
+      @data.size
+    end
+  end
+end
